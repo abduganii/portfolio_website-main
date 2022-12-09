b8e18969f8a0aebad5e5f5a8f277cb515ef9ad33
Abdugani Abduqaxxorov <83403180+abduganii@users.noreply.github.com>
2022-12-09 18:06:34 +0500

    Initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    title: 'Blogs website',
+    description: "Using React, Node.js, Express & MongoDB . The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+      image: '/images/1.jpg',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://blogs-front.vercel.app',
+    visit: 'https://github.com/abduganii',
+    id: 0,
+  },
+  {
+    title: 'Admin Panel',
+    description:"Using React, Node.js, Express & MongoDB. you'll get information about company. It's a CRUD Website",
+    image: '/images/2.jpg',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://google.com',
+    visit: 'https://nafagencyadmin.vercel.app',
+    id: 1,
+  },
+
+];
+
+export const TimeLineData = [
+  { year: 2021, text: 'Started my journey', },
+
+  { year: 2022, text: 'Got job in IT company', },
+  { year: 2022, text: 'Worked as a freelance developer', },
+
+];
